c6b2644d166a47e8e48552952500bfa0bb92effe
steinaro14 <steinar171@gmail.com>
2015-05-06 11:15:10 +0000

    First commit

---
+++ Wasd/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Wasd.Startup))]
+namespace Wasd
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
