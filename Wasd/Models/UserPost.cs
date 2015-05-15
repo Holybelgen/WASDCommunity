5405554f3029ae3467927d30a2a243b46a45668f
steinaro14 <steinar171@gmail.com>
2015-05-15 15:06:30 +0000

    sma

--- Wasd/Models/UserPost.cs
+++ Wasd/Models/UserPost.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
@@ -12,6 +13,7 @@ namespace Wasd.Models
         public int Id { get; set; }
         public string userName { get; set; }
         public string userID { get; set; }
+        [DataType(DataType.MultilineText)]
         public string content { get; set; }
         public string date { get; set; }
     }
